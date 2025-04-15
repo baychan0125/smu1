ff429168e39dc66bd59affd88dca4aede7b764c0
baychan0125 <yogurt7375@gmail.com>
2025-04-15 16:54:49 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for i in range(1, 10) :  
+    for a in range(2, 6) :  
+        print(f"{a} x {i} = {a*i}", end='\t')
+    print()
+
+print() 
+
+for i in range(1, 10) :
+    for b in range(6, 10) :
+        print(f"{b} x {i} = {b*i}", end='\t')
+    print()
